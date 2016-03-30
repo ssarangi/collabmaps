5c73498f7ea67a4ad33414feff96a823f13c2347
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-03-30 22:46:25 +0000

    Added code for chat and now will focus on the canvas and other parts needed

--- public/js/client/client.js
+++ public/js/client/client.js
@@ -9,20 +9,14 @@ $("#form").submit(function() {
     return false;
 });
 
-socket.on('chat message', function(msg) {
-    var s = $("#text").val();
-    $("#text").val(msg);
-});
+// socket.on('chat message', function(msg) {
+//     var s = $("#text").val();
+//     $("#text").val(msg);
+// });
 
-socket.on('user_connected', function(user) {
-   $("#text").append($('<li style="background:red">').text(user + " joined the chat")); 
-});
-
-function textbox_buttonpress(e, val) {
-    var c = String.fromCharCode(e.which);
-    socket.emit("chat_message", val + c);
-}
-
+// socket.on('user_connected', function(user) {
+//   $("#text").append($('<li style="background:red">').text(user + " joined the chat")); 
+// });
 
 ////////////////////////////// Maps Portion //////////////////////////////////////
 
@@ -34,3 +28,93 @@ var map = new mapboxgl.Map({
     center: [-74.50, 40], // starting position
     zoom: 9 // starting zoom
 });
+
+//////////////////////////// Chat Check ///////////////////////////////////////////
+(function () {
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    
+    $(function () {
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
+        };
+        
+        sendMessage = function (text) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side === 'left' ? 'right' : 'left';
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+        };
+        
+        $('.send_message').click(function (e) {
+            var text = getMessageText();
+            socket.emit("chat_message", username + ": " + text);
+            return sendMessage(text);
+        });
+        
+        $('.message_input').keyup(function (e) {
+            if (e.which === 13) {
+                var text = getMessageText();
+                socket.emit("chat_message", username + ": " + text);
+                return sendMessage(text);
+            }
+        });
+        
+        socket.on('chat message', function(msg) {
+            sendMessage(msg);
+        });
+        
+        socket.on('user_connected', function(user) {
+            sendMessage(user + " joined the chat");
+        });
+        
+        socket.on('user_disconnected', function(user) {
+            sendMessage(user + " disconnected from chat"); 
+        });
+        
+        socket.on('current_users', function(users) {
+            for (var i = 0; i < users.length; i++) {
+                sendMessage(users[i] + " in chat");
+            }
+        });
+        
+        // sendMessage('Hello Philip! :)');
+        
+        // setTimeout(function () {
+        //     return sendMessage('Hi Sandy! How are you?');
+        // }, 1000);
+        
+        // return setTimeout(function () {
+        //     return sendMessage('I\'m fine, thank you!');
+        // }, 2000);
+    });
+}.call(this));
--- server.js
+++ server.js
@@ -12,16 +12,34 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + "/html/index.html");
 });
 
+var users_id_map = {}
+
 io.on('connection', function(socket) {
     console.log('a user connected');
     
     socket.on('disconnect', function() {
-        console.log('user disconnected');
+        var username = users_id_map[this.id];
+        delete users_id_map[this.id];
+        socket.broadcast.emit('user_disconnected', username);
+        console.log(username + ' disconnected');
     });
     
     socket.on('user_connected', function(username) {
         socket.broadcast.emit('user_connected', username);
-    })
+        
+        var users = [];
+        for (var key in users_id_map) {
+          if (users_id_map.hasOwnProperty(key)) {
+            users.push(users_id_map[key]);
+          }
+        }
+        
+        if (users.length > 0) {
+            io.to(this.id).emit('current_users', users);
+        }
+
+        users_id_map[this.id] = username;
+    });
     
     socket.on('chat_message', function(msg) {
        socket.broadcast.emit('chat message', msg);
